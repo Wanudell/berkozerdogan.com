2bfb037ec48d205c31872e970d4868014b0f155b
Berk Ozerdogan <97380286+Wanudell@users.noreply.github.com>
2022-08-23 16:12:58 +0300

    Add files via upload

---
+++ API/myPersonelAPI/data/Personel.Entites/MainEntities/Comment.cs
@@ -0,0 +1,31 @@
+﻿using Personel.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personel.Entites.MainEntities
+{
+    [Table("Comments", Schema = "MainEntities")]
+    public class Comment : EntityBase
+    {
+        [Required]
+        public Guid PostId { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Post { get; set; }
+
+        [Required]
+        public DateTime SendDate { get; set; }
+    }
+}
